c1ba5083bea8ff03368ec69fa1817250072b7778
frankemesinwa <frankemesinwa@gmail.com>
2025-10-30 23:16:26 +0000

    header

--- src/app/campaigns/layout.tsx
+++ src/app/campaigns/layout.tsx
@@ -1,17 +1,21 @@
 'use client';
 
 import React from 'react';
+import { usePathname } from 'next/navigation';
 import Header from '@/components/campaigns/Header';
 import Footer from '@/components/campaigns/Footer';
 
 export default function CampaignsLayout({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
+  const hideHeaderFooter = pathname === '/campaigns';
+
   return (
     <div className="flex flex-col min-h-screen">
-      <Header />
+      {!hideHeaderFooter && <Header />}
       <main className="flex-grow">
         {children}
       </main>
-      <Footer />
+      {!hideHeaderFooter && <Footer />}
     </div>
   );
 }

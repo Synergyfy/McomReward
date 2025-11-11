eee5ffa388523b14fdaf6922d2ed1fb267af1126
Azeem <bljazeem@gmail.com>
2025-11-11 11:32:05 +0100

    moved the point balance component in points page to the wallet page and then commented out the wallet

--- src/components/customer/sidebar/index.tsx
+++ src/components/customer/sidebar/index.tsx
@@ -64,12 +64,12 @@ export default function CustomerSidebar({ isOpen }: CustomerSidebarProps) {
             Wallet
           </Link>
         </li>
-        <li className="mb-2">
+        {/* <li className="mb-2">
           <Link href="/points" className={linkClasses("/points")}>
             <Wallet className="mr-3" />
             Point
           </Link>
-        </li>
+        </li> */}
         <li className="mb-2">
           <Link href="/redemption" className={linkClasses("/redemption")}>
             <HandCoins className="mr-3" />
